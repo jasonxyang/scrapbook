d418a236ff94dc786f785fb158a8639bcec356a1
Jason Yang <jasony@kapwing.com>
2024-03-23 09:50:03 -0500

    chore: add document title input

---
+++ components/DocumentTitleInput.tsx
@@ -0,0 +1,17 @@
+import { ChangeEvent, memo, useCallback } from "react";
+import { TextInput } from "./generic/Input";
+import { useRecoilState } from "recoil";
+import titleAtom from "@/recoil/title";
+
+const DocumentTitleInput = () => {
+  const [currentTitle, setCurrentTitle] = useRecoilState(titleAtom);
+
+  return (
+    <div>
+      Document Title
+      <TextInput value={currentTitle} onValueChange={setCurrentTitle} />
+    </div>
+  );
+};
+
+export default memo(DocumentTitleInput);
--- components/generic/Input.tsx
+++ components/generic/Input.tsx
@@ -1,22 +1,28 @@
 import {
+  ChangeEvent,
   ChangeEventHandler,
   InputHTMLAttributes,
   forwardRef,
   memo,
+  useCallback,
 } from "react";
 
 type TextInputProps = {
   value: string;
-  onValueChange: ChangeEventHandler<HTMLInputElement>;
+  onValueChange: (value: string) => void;
   required?: InputHTMLAttributes<HTMLInputElement>["required"];
 };
 export const TextInput = memo(
   forwardRef<HTMLInputElement, TextInputProps>(
     ({ required, value, onValueChange }, ref) => {
+      const handleOnChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        onValueChange(e.currentTarget.value);
+      }, []);
+
       return (
         <input
           value={value}
-          onChange={onValueChange}
+          onChange={handleOnChange}
           type="text"
           ref={ref}
           required={required}
