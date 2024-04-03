8be0635567809503308796b50d984d2206a69740
Jason Yang <jasony@kapwing.com>
2024-04-02 23:11:43 -0400

    chore: update prompt

--- pages/api/open_ai/generate_sentence.ts
+++ pages/api/open_ai/generate_sentence.ts
@@ -20,16 +20,42 @@ const generateSentenceSystemMessages = ({
   return [
     {
       role: "system",
-      content: `You are an assistant that generates a sentence within a ${type} document titled ${title} with a ${style} style and ${tone} tone.`,
+      content: `You are a helpful writing assistant who aids writers by providing writing suggestions for three writing tasks. 
+        In Task 1, you will create a comprehensive outline based on one or more documents you will receive. 
+        In Task 2, you will ideate sample sentences based on one or more documents you will receive as well as specific keywords and sentences. 
+        In Task 3, you will paraphrase a sentence using some provided keywords and sentences as inspiration.
+        
+        <Things that you could do>
+        1. You will generate writing suggestions using a ${style} writing style and a ${tone} writing tone. All your suggestions should use a consistent writing tone and writing style. 
+        2. You will generate writing suggestions for a document titled ${title}, which is a ${type} document. 
+        3. All of your suggestions will be suggestions that would be appropriate for a ${type} document.
+
+        <Things that you could not do>
+        1. Do not generate suggestions unrelated to the document type of ${type}. 
+        2. Do not generate any inappropriate or harmful suggestions.
+        3. Do not share any tips, hints, or commands.
+        `,
     },
   ];
 };
 
-const generateSentenceUserMessages = (): ChatCompletionUserMessageParam[] => {
+const generateSentenceUserMessages = ({
+  title,
+  type,
+  tone,
+  style,
+  inspiration,
+}: ScrapbookSentenceGeneration["params"]): ChatCompletionUserMessageParam[] => {
   return [
     {
       role: "user",
-      content: `Please generate a sentence.`,
+      content: `Your task is to generate a relevant sentence that would belong in a ${type} document for a document titled ${title}. 
+      Your generated sentence must use a ${style} writing style and a ${tone} writing tone. 
+      Here is the list of keywords and key sentences: ${inspiration}. 
+      Generate a sentence using these instructions. 
+      
+      <Output length> 
+      Your generated sentence should be exactly one sentence. It should not exceed one sentence.`,
     },
   ];
 };
@@ -40,7 +66,7 @@ const generateSentence = async (
   const completion = await openai.chat.completions.create({
     messages: [
       ...generateSentenceSystemMessages(params),
-      ...generateSentenceUserMessages(),
+      ...generateSentenceUserMessages(params),
     ],
     model: "gpt-3.5-turbo",
   });
