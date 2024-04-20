c9fcac17efae3173391b08aac23a6cc109171cfd
Jason Yang <jasony@kapwing.com>
2024-04-20 16:04:36 -0400

    chore: cleanup"

---
+++ apps/google-docs-plugin/src/server/index.ts
@@ -0,0 +1,12 @@
+import { onOpen, openDialogTailwindCSS, openAboutSidebar } from './ui';
+
+import { getSelectedText, insertText } from './docs';
+
+// Public functions must be exported as named exports
+export {
+  onOpen,
+  openDialogTailwindCSS,
+  openAboutSidebar,
+  getSelectedText,
+  insertText,
+};
